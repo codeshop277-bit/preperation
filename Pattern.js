85f51f9c907dbc997a356f7f4e2ca775ed0b97cf
Codeshop277 <codeshop277@gmail.com>
2025-12-17 11:38:21 +0530

    Update Pattern.js

--- Pattern.js
+++ Pattern.js
@@ -149,15 +149,163 @@ let b = 4;
 // 12****21
 // 123**321
 // 12344321
-// for(let i=1; i<=b; i++){
+// for(let i=1; i<=a; i++){
 //     for (let j =1; j<=i; j++){
 //         process.stdout.write(j.toString())
 //     }
-//     for(let j=1; j<=2*b-2*i; j++){
-//         process.stdout.write("*")
+//     for(let j=1; j<=2*a-2*i; j++){
+//         process.stdout.write(" ")
 //     }
 //     for(let j=i; j>=1; j--){
 //         process.stdout.write(j.toString())
 //     }
 //     console.log()
 // }
+
+// let start = 1;
+// for(let i =1; i<=a; i++){
+//     for (let j = 1; j<=i; j++){
+//         process.stdout.write(start.toString())
+//         start = start +1
+//     }
+//     console.log()
+// }
+
+// for(let i =0; i<=a; i++){
+//     for (let j = 0; j<=i; j++){
+//         process.stdout.write(String.fromCharCode(65 + j) + " ")
+//     }
+//     console.log()
+// }
+
+// A B C D E 
+// A B C D
+// A B C
+// A B
+// A
+// for(let i =0; i<=a; i++){
+//     for (let j = 0; j<=a-i-1; j++){
+//         process.stdout.write(String.fromCharCode(65 + j) + " ")
+//     }
+//     console.log()
+// }
+
+// A 
+// B B 
+// C C C 
+// D D D D 
+// E E E E E 
+// F F F F F F 
+// for(let i=0; i<=a; i++){
+//     for(let j=0; j<=i; j++){
+//         process.stdout.write(String.fromCharCode(65 + i) + " ")
+//     }
+//     console.log()
+// }
+
+//   A   
+//   ABA
+//  ABCBA
+// ABCDCBA
+// for(let i=0; i<b; i++){
+//     //space/stars
+//     for(let j=0; j< b-i-1; j++){
+//         process.stdout.write(" ")
+//     }
+//      //chars
+//      let breakpoint = (2*i+1)/2;
+//     for(let j=0; j< 2*i+1; j++){
+//         if(j<breakpoint){
+//         process.stdout.write(String.fromCharCode(65 + j))
+//         }else{
+//         process.stdout.write(String.fromCharCode(65 + (2*i-j)))
+//         }
+//     }
+//      //space/stars
+//     for(let j=0; j< b-i-1; j++){
+//         process.stdout.write(" ")
+//     }
+//     console.log()
+// }
+
+// E
+// DE
+// CDE
+// BCDE
+// ABCDE
+// for(let i=0; i<=b; i++){
+//         let start = b-i;
+//     for(let j=0; j<=i ; j++){
+//         process.stdout.write(String.fromCharCode(65 + start))
+//         start++
+//     }
+//     console.log()
+// }
+
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+
+// for(let i=0; i<a; i++){
+//     //stars
+//     for(let j=0; j<a-i; j++){
+//         process.stdout.write("*")
+//     }
+//     //spaces
+//     for(let j=0; j<2*i; j++){
+//         process.stdout.write(" ")
+//     }
+//     //stars
+//     for(let j=0; j<a-i; j++){
+//         process.stdout.write("*")
+//     }
+//     console.log()
+// }
+
+// for(let i=1; i<=a; i++){
+//     //stars
+//     for(let j=1; j<=i; j++){
+//         process.stdout.write("*")
+//     }
+//     //spaces
+//     for(let j=1; j<=2*a-2*i; j++){
+//         process.stdout.write(" ")
+//     }
+//     //stars
+//     for(let j=1; j<=i; j++){
+//         process.stdout.write("*")
+//     }
+//     console.log()
+// }
+let n = 5; 
+for(let i=1; i<=2*n-1; i++){
+    let starCount = n - i + 1; 
+    let spaceCount = 2*i - 2;
+
+    //adjust for second half
+    if(i > n){
+        starCount = i - n + 1;
+        spaceCount = 4*n - 2*i - 2;
+    }
+    
+    //left stars
+    for(let j=1; j<=starCount; j++){
+        process.stdout.write("*")
+    }
+    //spaces
+    for(let j=1; j<=spaceCount; j++){
+        process.stdout.write(" ")
+    }
+    //right stars
+    for(let j=1; j<=starCount; j++){
+        process.stdout.write("*")
+    }
+    console.log()
+}
