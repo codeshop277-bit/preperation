5fd83301d2471fdd6bfb9899347024ec31fca724
Codeshop277 <codeshop277@gmail.com>
2026-01-01 09:09:32 +0530

    Arrays

--- DSA/Arrays/Array.js
+++ DSA/Arrays/Array.js
@@ -687,7 +687,7 @@ function MajorityElement(nums) {
     }
     return ans;
 }
-console.log(MajorityElement([3, 2, 3, 1, 2, 3, 3, 2]))
+// console.log(MajorityElement([3, 2, 3, 1, 2, 3, 3, 2]))
 
 function MajorityElementMoore(nums) {
     // in an array of size n, there can be atmost 2 elements with freq > n/3
@@ -726,4 +726,113 @@ function MajorityElementMoore(nums) {
     }
     return ans;
 }
-console.log(MajorityElementMoore([3, 2, 3, 1, 2, 3, 3, 2]))
+// console.log(MajorityElementMoore([3, 2, 3, 1, 2, 3, 3, 2]))
+
+function ThreeSum(nums) {
+    let n = nums.length;
+    let set = new Set();
+    let ans = [];
+
+    for(let i=0; i<n; i++){
+        for(let j=i+1; j<n; j++){
+            for(let k=j+1; k<n; k++){
+                if(nums[i] + nums[k] + nums[j] == 0){
+                    let triplet = [nums[i], nums[j], nums[k]];
+                    set.add(triplet.sort())
+                }
+            }
+        }
+    }
+    for (let st of set){
+        ans.push(st)
+    }
+    return ans
+}
+// console.log(ThreeSum([-1, 0, 1, 2, -1, -4]))
+
+
+function ThreeSumBetter(nums) {
+    let n = nums.length;
+    let set = new Set();
+    let ans = [];
+
+    for(let i=0; i<n; i++){
+    let map = new Map();
+        for(let j=i+1; j<n; j++){
+            let third = -(nums[i] + nums[j]);
+            if(map.has(third)){
+                if(nums[i] + nums[j] + third == 0){
+                    let triplet = [nums[i], nums[j], third];
+                    set.add(triplet.sort())
+                }
+            }
+            map.set(nums[j], j)
+                
+        }
+    }
+    for (let st of set){
+        ans.push(st)
+    }
+    return ans
+}
+// console.log(ThreeSumBetter([-1, 0, 1, 2, -1, -4]))
+
+function ThreeSumOptimal(nums) {
+    let n = nums.length;
+    let ans = [];
+    //using two pinter approach, sort the array first then fix one element and use two pointer for other two elements for example if we fix i, then left = i+1 and right = n-1 and move accordingly
+    nums.sort((a,b)=>a-b) //T>c - o(nlogn)
+    //Also if nums[i] == nums[i-1] continue to avoid duplicates similarly for left and right pointers
+    for(let i=0; i<n; i++){
+        let j = i+1; let k = n-1;
+        if(i> 0 && nums[i] == nums[i-1]) continue;
+        while(j<k){
+            let sum = nums[i] + nums[j] + nums[k];
+            if(sum <0){
+                j++;
+            }else if (sum > 0){
+                k--;
+            }else {
+                ans.push([nums[i], nums[j], nums[k]]);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
+            }
+        }
+    }
+   
+    return ans
+}
+// console.log(ThreeSumBetter([-1, 0, 1, 2, -1, -4]))
+
+function FourSum(nums, target) {
+    let n = nums.length;
+    let ans = [];
+    nums.sort((a,b) => a-b)
+
+    for(let i =0; i<n; i++){
+        if(i>0 && nums[i] == nums[i-1]) continue;
+        for(let j=i+1; j<n; j++){
+            let k = j+1;
+            let l = n-1;
+            if(j> i+1 && nums[j] == nums[j-1]) continue
+            while(k<l){
+                let sum = nums[i] + nums[j] + nums[k] + nums[l];
+                if(sum<target){
+                    k++
+                }else if (sum>target){
+                    l--;
+                }else{
+                    ans.push([nums[i], nums[j], nums[k], nums[l]]);
+                    k++
+                    l--;
+                    while(k<l && nums[k] == nums[k-1]) k++;
+                    while(k<l && nums[l] == nums[l+1]) l--;
+                }
+            }
+        }
+    }
+    return ans
+}
+console.log(FourSum([1, -2, 3, 5, 7, 9], 7))
