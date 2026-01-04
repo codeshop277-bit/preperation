acd53b0218838a8af1c8926c077835d9381ef803
Codeshop277 <codeshop277@gmail.com>
2026-01-04 08:10:38 +0530

    Binary Search

--- DSA/BinarySearch/Search.js
+++ DSA/BinarySearch/Search.js
@@ -80,7 +80,7 @@ function Floor(nums, target){
     return floor
 }
 
-console.log(Floor([1, 2, 4, 6, 8, 10], 5));//{4,6}
+// console.log(Floor([1, 2, 4, 6, 8, 10], 5));//{4,6}
 
 
 function Ceil(nums, target){
@@ -110,4 +110,60 @@ function FloorAndCeil(nums, target){
 }
 
 // Test cases
-console.log(FloorAndCeil([1, 2, 4, 6, 8, 10], 5));  // {floor: 4, ceil: 6}
+// console.log(FloorAndCeil([1, 2, 4, 6, 8, 10], 5));  // {floor: 4, ceil: 6}
+
+function firstOcccurance(nums, target){
+    let n = nums.length;
+    let low = 0;
+    let high = n-1;
+    let first = -1;
+
+    while (low<=high){
+        let mid = Math.floor((low+high)/2);
+        if(nums[mid] == target){
+            first = mid;
+            high = mid-1;
+        }else if (nums[mid] > target){
+            high = mid -1;
+        }else {
+            low = mid + 1;
+        }
+    }
+    return first
+};
+// console.log(firstOcccurance([1,2,2,2,3,4,5], 15));
+
+function LastOcccurance(nums, target){
+    let n = nums.length;
+    let low = 0;
+    let high = n-1;
+    let last = -1;
+
+    while(low<=high){
+        let mid = Math.floor((low + high)/2);
+        if(nums[mid] === target){
+            last = mid;
+            low = mid+1
+        }else if(nums[mid] > target){
+            high = mid-1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return last
+}
+
+// console.log(LastOcccurance([1,2,2,2,3,4,5], 15));
+
+function firstAndLastOcccurance(nums, target){
+    return [firstOcccurance(nums, target), LastOcccurance(nums, target)]
+}
+// console.log(firstAndLastOcccurance([1,2,2,2,3,4,5], 2));
+function CountOcccurance(nums, target){
+    let first = firstOcccurance(nums, target);
+    if(first === -1) return 0;
+    let last = LastOcccurance(nums, target);
+
+    return last-first +1; //Since it is a sorted array, all occurances will be in between first and last index, so the last - first +1 will give the count of occurances
+}
+// console.log(CountOcccurance([1,2,2,2,3,4,5], 2));
