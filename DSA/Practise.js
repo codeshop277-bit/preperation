f7237001583bdf7adace25f0d67ebd4fdfd00346
Codeshop277 <codeshop277@gmail.com>
2026-01-06 21:48:46 +0530

    Pratise

--- DSA/Practise.js
+++ DSA/Practise.js
@@ -1,4 +1,36 @@
+function LongestSubArray(nums,k){
+    let n = nums.length;
+    let sum = 0;
+    let map = new Map();
+    let maxLen = 0;
+    let start = 0;
+    let end = -1;
+    map.set(0,-1)
+    for(let i=0; i<n; i++){
+        sum+=nums[i];
+        if(sum == k){
+            if(i+1 > maxLen){
+                start = 0;
+                end = i
+                maxLen = i+1
+            }
+        }
+        if(map.has(sum-k)){
+            let prefix  = i-(map.get(sum-k))
+            if(prefix > maxLen){
+                maxLen = prefix 
+                end = i
+                start = map.get(sum-k) +1
+            }
+        }
 
+        if(!map.has(sum)){
+            map.set(sum, i)
+        }
+    }
+    return nums.slice(start, end+1)
+}
+console.log(LongestSubArray([2,3,1,5,1,1,2,1], 5))
 
 function Kadene(nums,k){
     let n = nums.length;
