3561d203b814f54909d76d26397cf84bb1e6d00c
Codeshop277 <codeshop277@gmail.com>
2026-01-11 11:28:51 +0530

    String

--- DSA/String/String.js
+++ DSA/String/String.js
@@ -191,4 +191,32 @@ function atoi(s){
     }
     return sign*res
 }
-console.log(atoi("4193 with words"))
+// console.log(atoi("4193 with words"))
+
+function LongestPalindrome(s){
+    let start = 0;
+    let maxLen = 0;
+
+    function expandAroundCenter(left, right){
+        while(left >=0 && right <=s.length && s[left] == s[right]){
+            left--;
+            right++
+        }
+        return right -left -1
+    }
+
+    for(let i=0; i<s.length; i++){
+        let len1 = expandAroundCenter(i, i);
+        let len2 = expandAroundCenter(i, i+1);
+
+        let len = Math.max(len1, len2)
+
+        if(len > maxLen){
+            maxLen = len
+            start = i-Math.floor((len -1)/2)
+        }
+    }
+    return s.substring(start, start+maxLen)
+
+}
+ console.log(LongestPalindrome("bababd"))
