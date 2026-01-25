358bdd1fe89915d9bc0709c0cb591ef975289dfe
Codeshop277 <codeshop277@gmail.com>
2026-01-25 07:02:49 +0530

    String

--- DSA/String/String.js
+++ DSA/String/String.js
@@ -337,4 +337,49 @@ function rabinKarp(text, pattern) {
   return result;
 }
 
-rabinKarp('ABCABC', 'ABC');
+// rabinKarp('ABCABC', 'ABC');
+
+function computeLPS(pattern){
+    const lps = Array(pattern.length).fill(0)
+    let len = 0;
+    let i = 1;
+    while(i<pattern.length){
+        if(pattern[i] == pattern[len]){
+            len++
+            lps[i] = len
+            i++
+        }else{
+            if(len != 0){
+                len = lps[len-1]
+            }else{
+                lps[i] = 0
+                i++
+            }
+        }
+    }
+    return lps
+}
+function KPSALGO(text, pattern){
+    const lps = computeLPS(pattern)
+    const result = []
+    let i=0;
+    let j =0;
+
+    while(i<text.length){
+        if(text[i] == pattern[j]){
+            i++
+            j++
+        }
+        if(j== pattern.length){
+            result.push(i-j)
+            j=lps[j-1]
+        }else if(i<text.length && text[i] !== pattern[j]){
+            if(j!=0){
+                j = lps[j-1]
+            }else{
+                i++
+            }
+        }
+    }
+    return result
+}
