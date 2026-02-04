27ae62d998ba5053589a65dd9d6b52630b3dfc13
Codeshop277 <codeshop277@gmail.com>
2026-02-04 15:38:26 +0530

    Practise

--- DSA/Practise.js
+++ DSA/Practise.js
@@ -1,16 +1,18 @@
-function Pattern(a) {
-    for (let i = 1; i <= 2 * a - 1; i++) {
-        for (let j = 1; j <= 2 * a - 1; j++) {
-            let top = i;
-            let left = j;
-            let right = 2 * a - j;
-            let bottom = 2 * a - i;
-            let val = Math.min(Math.min(top, bottom), Math.min(left, right));
-            process.stdout.write(`${a - val + 1}`)
-        }
-        console.log()
+function Pattern(nums) {
+    const map = new Map();
+    for(let i=0; i<nums.length; i++){
+        map.set(nums[i], (map.get(nums[i]) ||0)+1);
     }
-    return "";
+    let maxFreq = 0;
+    let result = -Infinity
+    for (const [key, value] of map){
+        if((value > maxFreq) || (value == maxFreq && key < result)){
+            maxFreq = value;
+            result = key;
+        }
+    }
+    return {maxFreq, result}
+    
 }
 
-console.log(Pattern(5)); //3
+console.log(Pattern([1,1,2,3,4,5,2,3,23,2,2,3])); //3
