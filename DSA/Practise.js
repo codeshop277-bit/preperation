f05d1aa98ba055046e807c5f221084662bdfbb21
Codeshop277 <codeshop277@gmail.com>
2026-02-05 21:39:01 +0530

    Sort

--- DSA/Practise.js
+++ DSA/Practise.js
@@ -1,18 +1,38 @@
-function Pattern(nums) {
-    const map = new Map();
-    for(let i=0; i<nums.length; i++){
-        map.set(nums[i], (map.get(nums[i]) ||0)+1);
-    }
-    let maxFreq = 0;
-    let result = -Infinity
-    for (const [key, value] of map){
-        if((value > maxFreq) || (value == maxFreq && key < result)){
-            maxFreq = value;
-            result = key;
+function merge(left, right) {
+    const result = [];
+    let i = 0;
+    let j = 0;
+
+    while(i<left.length && j<right.length){
+        if(left[i] < right[j]){
+            result.push(left[i]);
+            i++
+        }else{
+            result.push(right[j]);
+            j++
         }
     }
-    return {maxFreq, result}
     
+    return result.concat(left.slice(i)).concat(right.slice(j));
 }
+function SOrt(arr) {
+    if(arr.length <=1) return arr
+   let mid = Math.floor(arr.length/2);
+   let left = arr.slice(0, mid);
+   let right = arr.slice(mid)
+   return merge(SOrt(left), SOrt(right))
+}
+console.log(SOrt([5, 4, 3, 2, 1]))
 
-console.log(Pattern([1,1,2,3,4,5,2,3,23,2,2,3])); //3
+function InsertionSort(arr){
+    for(let i=0; i<=arr.length; i++){
+        let j=i;
+        while(j>0 && arr[j-1] > arr[j]){
+            [arr[j-1], arr[j]] = [arr[j], arr[j-1]];
+            j--;
+            console.log(arr)
+        }
+    }
+    return arr
+}
+console.log(InsertionSort([5, 4, 3, 2, 1]))
