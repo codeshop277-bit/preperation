f1b9a1013fba1fbf035411d558b04fa96adf11de
Codeshop277 <codeshop277@gmail.com>
2026-02-09 21:51:24 +0530

    Practice

--- DSA/Practise.js
+++ DSA/Practise.js
@@ -1,5 +1,15 @@
-function fibonacci(n) {
-    if(n<=1) return n;
-    return fibonacci(n-1) + fibonacci(n-2)
+function missingNo(nums) {
+    let max = 0;
+    let count = 0;
+    for (let i = 0; i <= nums.length; i++) {
+        if(nums[i] == 1){
+            count++;
+        }else{
+            max = Math.max(max, count);
+            count = 0;
+        }
+    }
+    return max;
 }
-console.log(fibonacci(10))
+
+console.log(missingNo([1,1,0,1,1,1,0]));
