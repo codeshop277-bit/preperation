9ac49e8ed6c9eccc22a3ae2e76965a26b34c8173
Codeshop277 <codeshop277@gmail.com>
2026-02-11 17:02:01 +0530

    Practice

--- DSA/Practise.js
+++ DSA/Practise.js
@@ -1,22 +1,44 @@
-class ServerConfig{
-    constructor(port, host){
-        this.port = port
-    }
-}
+function SetMatrixZeroesOptimal(matrix) {
+    let rows = matrix.length
+    let cols = matrix[0].length
+    let firstRowHasZero = false
+    let firstColHasZero = false
 
-class ServerConfigBuilder{
-    constructor(host, port){
-        this.host = host
+    for (let i = 0; i < rows; i++) {
+        if (matrix[i][0] == 0) {
+            firstColHasZero = true
+        }
     }
-    enableSSl(){
-        this.ssl = true
-        return this
+    for (let i = 0; i < cols; i++) {
+        if (matrix[0][i] == 0) {
+            firstRowHasZero = true
+        }
     }
-    build(){
-        return new ServerConfig(this)
+    for(let i=1; i< rows; i++){
+        for(let j=1; j<cols; j++){
+            if(matrix[i][j] == 0){
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+            }
+        }
     }
+     for(let i=1; i< rows; i++){
+        for(let j=1; j<cols; j++){
+            if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                matrix[i][j] = 0
+            }
+        }
+    }
+    if(firstColHasZero){
+        for(let i=0; i<rows; i++){
+            matrix[i][0] = 0
+        }
+    }
+    if(firstRowHasZero){
+        for(let i=0; i<cols; i++){
+            matrix[0][i] = 0
+        }
+    }
+return matrix
 }
-
-const server = new ServerConfigBuilder(2000, 2525)
-.enableSSl()
-.build();
+console.log(SetMatrixZeroesOptimal([[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]))
