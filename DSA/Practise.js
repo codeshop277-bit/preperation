5156ff78a75bf26c6161caf957d4207b78fca98c
Codeshop277 <codeshop277@gmail.com>
2026-02-17 21:49:08 +0530

    Practice

--- DSA/Practise.js
+++ DSA/Practise.js
@@ -1,20 +1,49 @@
-function Pascal(r,c){
-    let row = r-1;
-    let col = c-1;
-    let ans = 1;
-    for(let i=0; i<col; i++){
-        ans = Math.floor(ans *(row-i)/(i+1))
+function Search(nums, k){
+    let low = 0;
+    let high = nums.length-1;
+    while(low<=high){
+        let mid = Math.floor((low+high)/2);
+        if(nums[mid] == k) return mid
+        if(nums[low] <= nums[mid] ){
+            if(nums[low]<=k && k<= nums[high]){
+                high = mid-1;
+            }else{
+                low = mid+1
+            }
+        }else{
+            if(nums[mid] <=k && k<=nums[high]){
+                low = mid+1
+            }else{
+                high = mid -1
+            }
+        }
     }
-    return ans
+    return false
 }
-function PrintRow(n){
-    let triangle = [1];
-    let ans = 1;
-    for(let i=1; i<n; i++){
-        ans = Math.floor(ans * (n-i) / i)
-        triangle.push(ans)
+console.log(Search([2,5,6,0,0,1,2], 0))
+
+function Min(nums){
+    let low = 0;
+    let high = nums.length-1;
+    let min = Infinity
+    let index = -1;
+    while(low<=high){
+        let mid = Math.floor((low+high)/2)
+        if(nums[low] <= nums[mid]){
+            if(nums[low] < min){
+                min = nums[low];
+                index = low;
+            }
+
+                low = mid+1
+        }else{
+            if(nums[mid]< min){
+                min = nums[mid]
+                index = mid
+            }
+            high  = mid -1
+        }
     }
-    return triangle
+    return {index, min}
 }
-console.log(Pascal(3,2))
-console.log(PrintRow(5))
+console.log(Min([4,5,6,7,0,1,2,3]))
