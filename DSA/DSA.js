8e86ccbeb62cfa460a271f0b7eb7b0fed155ec0c
Codeshop277 <codeshop277@gmail.com>
2026-02-26 17:11:07 +0530

    Practise

--- DSA/DSA.js
+++ DSA/DSA.js
@@ -1,14 +1,18 @@
-function MaxDepth(str){
-    let count = 0;
-    let max = 0;
-    for(let char of str){
-        if(char == '('){
-            count++
-            max = Math.max(count, max)
-        }else if(char == ")"){
-            count--
+function Beauty(str){
+    
+    let total =0;
+    for(let i=0; i< str.length; i++){
+        let freq = {}
+        for(let j=i; j< str.length; j++){
+            const ch = str[j]
+            freq[ch] = (freq[ch] || 0) +1
+            const values = Object.values(freq)
+            const max = Math.max(...values)
+            const min = Math.min(...values)
+            total += (max-min)
         }
     }
-    return max
+    return total
+
 }
-console.log(MaxDepth("(1)+((2))+(((3())))"))
+console.log(Beauty("aabcbaa"))
