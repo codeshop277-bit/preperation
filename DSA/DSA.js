3047effcf10e0927025f7db4caadda73cdc337da
Codeshop277 <codeshop277@gmail.com>
2026-03-07 11:00:09 +0530

    Scaling

--- DSA/DSA.js
+++ DSA/DSA.js
@@ -1,23 +1,71 @@
-function StartOfLoop(arr){
-    let head = arr;
-    let slow = head;
-    let fast = head;
-        let count = 0
+function Sort(ll){
+    let niddle = findMiddle(ll)
+    let right = FindMiddle.next
+    FindMiddle.next = null
+    let left = head
 
-    while(slow && fast && fast.next != null){
-        slow = slow.next
-        fast = fast.next.next
-       if(slow == fast){
-        slow = slow.next
-        count++
-        while(slow != fast){
-            slow = slow.next
-            count++
+    let leftNode = Sort(left)
+    let rightNode = Sort(right)
+    return mergeSortLL(leftNode, rightNode)
+}
+
+function mergeSort(l1, le2){
+    let temp = new Node(-1)
+    let current = temp
+    while(l1 != null && l2 != null){
+        if(l1.data <= l2.data){
+            current.next = l1
+            l1 = l1.next
         }
-        return count
-       }
     }
 }
 
-//
-Build  a gold price tracker app. It should have only one landing page. The landing page will display the gold /gm and /soverign rates for the past 5 days in tabular form. Rates should be in INR. Top right of the table should have avg proce of the week and how much it has been increased or decreased from previous day. Use rich ui like framer motion. UI should look rich. And best to have in yellow gradients. Use nextjs for ui and no need of separate backend code. Come up with a plan of what you are going to build before executing it
+function SOrt012(ll){
+    let zero = new Node(-1)
+    let one = new Node(-1)
+    let two = new Node(-1)
+
+    let zeroNode = zero
+    let  oneNode = one
+    let twoNode = two
+
+    while(temp != null){
+        if(temp.data == 0){
+            zero.next = temp
+            zero = zero.next
+        }else if(temp.data == 1){
+            one.next = temp
+            one = one.next
+        }else{
+            two.next = temp
+            two = two.next
+        }
+    }
+    zero.next = oneNode.next ? oneNode.next : twoNode.next
+    one.next = twoNode.next
+    two.next = null
+}
+function add(){
+    let head1;
+    let head2;
+    let carry = 0
+
+    while(head1 != null && head2 != null){
+        let sum = carry
+        if(head1) sum+=head1.data
+        if(head2) sum+=head2.data
+
+        carry = Math.floor(sum/100)
+        let newNode = new Node(sum %10)
+        current.next = newNode
+        current = current.next
+
+        if(head2) head2 = head2.next
+        if(head1) head1 = head1.next
+
+    }
+    if(carry >0){
+        let newNode = new Node(1)
+        newNode.next = current
+    }
+}
